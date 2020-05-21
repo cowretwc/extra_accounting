c6c5ceaba9671be969dd66ae255eae131524b125
cowretwc <cowretwc@gmail.com>
2020-05-21 14:38:04 +0700

    Push Red Color

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':"Exstra Accounting",
+    'summary':'Extra Accounting Tools',
+    'description':'',
+    'category':'Extra Tools',
+    'author':'Bluespada',
+    'depends':[
+        'base',
+        'account',
+    ],
+    'data':[
+        'view/account_payment.xml',
+        'view/account_bank.xml',
+
+        'view/refunds.xml',
+    ],
+}
